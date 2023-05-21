96bc33581f116e87e0b74a2372dae6d0d53cf919
nadiamanasterska <nadiamanast@gmail.com>
2023-05-21 22:51:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+import Student from './Student'
+import { v4 as uuidv4 } from 'uuid';
+
+const API_STUDENTS = 'http://127.0.0.1:8000/students/'
+
+
+function App() {
+
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    fetch(API_STUDENTS).then((response) => console.log(response.json().then((data) => setStudents(data.students))))
+  }, [])
+
+
+  return (
+    <div className='app-container'>
+      <h3>Students: </h3>
+      {students.map((student) => (
+        <Student
+          key={uuidv4()}
+          name={student.first_name}
+          surname={student.last_name}
+        />
+      ))}
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Student.jsx
@@ -0,0 +1,10 @@
+const Student = ({name, surname}) => {
+  return (  
+    <div className="student-container">
+      <p>name: {name}</p>
+      <p>surname: {surname}</p>
+    </div>
+  );
+}
+ 
+export default Student;
